eae0559751b44cd5ac781bb8e4ce46c6c9f61957
wansongtao <18101837209@163.com>
2022-07-04 17:24:00 +0800

    feat:数组的foreach方法实现

---
+++ chapter3/foreach.ts
@@ -0,0 +1,26 @@
+/**
+ * 为数组中的每一项有效值执行一次callback函数
+ * @param arr 
+ * @param callback 
+ * @returns 
+ */
+const foreach = <T = any>(arr: T[], callback: (element: T, index?: number, array?: T[]) => void) => {
+  const len = arr.length;
+
+  if (len === 0) {
+    return;
+  }
+
+  let k = 0;
+  do {
+    if (arr[k] === undefined) {
+      k++;
+      continue;
+    }
+
+    callback(arr[k], k, arr);
+    k++;
+  } while(k < len);
+};
+
+const foreachRes = foreach([1, 2, 3], (val) => console.log(val));
