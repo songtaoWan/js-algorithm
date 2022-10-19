d514309e697b418af544c6f1b07fc6d3924149c0
wansongtao <18101837209@163.com>
2022-10-19 15:12:10 +0800

    fix:处理有向图存在不连通的顶点时深度优先搜索算法不能访问到的问题

--- chapter12/graph.ts
+++ chapter12/graph.ts
@@ -6,14 +6,14 @@ interface IColor {
 
 // 获取对象中的某个值
 type IValue<T = unknown> = {
-  [P in keyof T]: T[P]
+  [P in keyof T]: T[P];
 }[keyof T];
 
 class Graph<T extends number | string | symbol> {
   private isDirected: boolean;
   private vertices: T[];
   private adjList: Record<T, T[]> | {};
-  private static Colors: IColor = {white: 0, gray: 1, black: 2};
+  private static Colors: IColor = { white: 0, gray: 1, black: 2 };
 
   constructor(isDirected = false) {
     this.isDirected = isDirected;
@@ -31,8 +31,8 @@ class Graph<T extends number | string | symbol> {
 
   /**
    * 添加边，有向图v=>w，无向图v=>w w=>v
-   * @param v 
-   * @param w 
+   * @param v
+   * @param w
    */
   addEdge(v: T, w: T) {
     if (!this.vertices.includes(v)) {
@@ -68,7 +68,7 @@ class Graph<T extends number | string | symbol> {
 
   /**
    * 给每个顶点上色
-   * @returns 
+   * @returns
    */
   private initColor(): Record<T, IValue<IColor>> {
     const vertices = this.vertices;
@@ -84,9 +84,12 @@ class Graph<T extends number | string | symbol> {
    * 广度优先搜索算法
    * @param startVertex 起始顶点
    * @param callback 一个顶点被探索完后执行的回调函数
-   * @returns 
+   * @returns
    */
-  breadthFirstSearch(startVertex: T = this.vertices[0], callback?: (v: T) => void) {
+  breadthFirstSearch(
+    startVertex: T = this.vertices[0],
+    callback?: (v: T) => void
+  ) {
     const vertices = this.getVertices();
     if (vertices.length < 2 || !vertices.includes(startVertex)) {
       return;
@@ -100,7 +103,7 @@ class Graph<T extends number | string | symbol> {
     verticesColor[startVertex] = Graph.Colors.gray;
     queue.push(startVertex);
 
-    while(queue.length) {
+    while (queue.length) {
       const vertex = queue.shift() as T;
 
       // 处理没有邻接表的顶点
@@ -116,7 +119,7 @@ class Graph<T extends number | string | symbol> {
       const list = adjList[vertex];
       for (let i = 0; i < list.length; i++) {
         const v = list[i];
-        
+
         if (verticesColor[v] === Graph.Colors.white) {
           verticesColor[v] = Graph.Colors.gray;
           queue.push(v);
@@ -132,8 +135,8 @@ class Graph<T extends number | string | symbol> {
 
   /**
    * 深度优先搜索算法
-   * @param callback 
-   * @returns 
+   * @param callback
+   * @returns
    */
   depthFirstSearch(callback?: (v: T) => void) {
     const vertices = this.getVertices();
@@ -167,7 +170,13 @@ class Graph<T extends number | string | symbol> {
       verticesColor[v] = Graph.Colors.black;
     };
 
-    dfs(vertices[0]);
+    for (let loop = 0; loop < vertices.length; loop++) {
+      const v = vertices[loop];
+
+      if (verticesColor[v] === Graph.Colors.white) {
+        dfs(v);
+      }
+    }
   }
 
   toString() {
@@ -177,9 +186,11 @@ class Graph<T extends number | string | symbol> {
       return '图不存立，只有顶点没有边';
     }
 
-    return vertices.map((val) => {
-      return `${val[0]} -> ${(val[1] as T[]).join(', ')}`;
-    }).join('\n');
+    return vertices
+      .map((val) => {
+        return `${val[0]} -> ${(val[1] as T[]).join(', ')}`;
+      })
+      .join('\n');
   }
 }
 
@@ -194,6 +205,8 @@ graph1.addVertex('g');
 graph1.addVertex('h');
 graph1.addVertex('i');
 graph1.addVertex('j');
+graph1.addVertex('x');
+// graph1.addVertex('j');
 
 graph1.addEdge('a', 'b');
 graph1.addEdge('a', 'c');
@@ -206,6 +219,8 @@ graph1.addEdge('d', 'g');
 graph1.addEdge('d', 'h');
 graph1.addEdge('e', 'i');
 graph1.addEdge('i', 'j');
+graph1.addEdge('x', 'a');
+graph1.addEdge('x', 'b');
 
 console.log(graph1.toString());
 
