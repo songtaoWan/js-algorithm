e608507f70f85f6a052597e2aa814e347d6e957b
wansongtao <18101837209@163.com>
2022-10-21 17:46:38 +0800

    feat:插入排序实现

---
+++ chapter13/insertionSort.ts
@@ -0,0 +1,35 @@
+import { compare, getChaoticArray } from './test';
+
+export default function insertionSort<T = unknown>(
+  arr: T[],
+  compareFn: (a: T, b: T) => boolean
+) {
+  const length = arr.length;
+  if (!length) {
+    return [];
+  }
+
+  // 将数组的每一项与其前面的每一个元素比较
+  for (let i = 1; i < length; i++) {
+    let prevIdx = i - 1;
+    const currVal = arr[i];
+
+    while(prevIdx >= 0 && compareFn(arr[prevIdx], currVal)) {
+      arr[prevIdx + 1] = arr[prevIdx];
+      prevIdx--;
+    }
+
+    prevIdx++;
+    arr[prevIdx] = currVal;
+  }
+
+  return arr;
+}
+
+const cha1 = getChaoticArray();
+
+const r1 = insertionSort(cha1, compare);
+console.log(r1);
+const r2 = insertionSort(cha1, (a, b) => b > a);
+console.log(r2);
+
