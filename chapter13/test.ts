edf2d4e7e79e5ca29e74da55331490485b552f9a
wansongtao <18101837209@163.com>
2022-10-21 15:06:04 +0800

    feat:实现一个生成乱序数组的方法，方便测试排序算法

---
+++ chapter13/test.ts
@@ -0,0 +1,40 @@
+/**
+ * 测试排序算法
+ */
+
+/**
+ * a大于b返回true
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+export const compare = <T = unknown>(a: T, b: T) => {
+  return a > b;
+};
+
+/**
+ * 生成一个乱序数组
+ * @param len 数组长度
+ * @param min 
+ * @param max 
+ * @returns 
+ */
+export const getChaoticArray = (len = 10, min = 10, max = 100) => {
+  if (len < 0) {
+    len = Math.abs(len);
+  }
+  if (~~len !== len) {
+    len = len | 0;
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  const arr: number[] = [];
+  for (let i = 0; i < len; i++) {
+    const randomNum = Math.floor(Math.random() * (max - min) + min);
+    arr.push(randomNum);
+  }
+
+  return arr;
+};
