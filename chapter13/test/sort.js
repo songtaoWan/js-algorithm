964dc4911a5d3a81cc68343b3eb820e57aa67f90
wansongtao <18101837209@163.com>
2022-11-01 10:31:43 +0800

    feat:基数排序js版

--- chapter13/test/sort.js
+++ chapter13/test/sort.js
@@ -388,3 +388,78 @@ const bucketSort = (arr, getValue, bucketSize, isAsc = true) => {
 
   return newArr;
 };
+
+/**
+ * 基数排序，不改变原数组。
+ * @param {any[]} arr
+ * @param {(a) => number} getValue 获取元素的值，必须返回整数
+ * @param isAsc 是否升序，默认是
+ * @returns
+ */
+const radixSort = (arr, getValue, isAsc = true) => {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  /**
+   * 将数组按有效位排序
+   * @param arr
+   * @param significantDigit 有效位，例如：个位 1，十位 10
+   * @param getValue
+   * @returns
+   */
+  const countsRadixSort = (arr, significantDigit, getValue, isAsc = true) => {
+    const radixs = [];
+    arr.forEach((val) => {
+      const realValue = getValue(val);
+      const absValue = Math.abs(realValue);
+      // 计算有效位，例如：60的十位有效位6
+      let i = Math.floor((absValue / significantDigit) % 10);
+
+      // 将负数保存到数组索引10-19的十个桶中
+      if (realValue < 0) {
+        i += 10;
+      }
+
+      if (!radixs[i]) {
+        radixs[i] = [];
+      }
+      radixs[i].push(val);
+    });
+
+    const results = [];
+    radixs.forEach((val, i) => {
+      if (isAsc) {
+        if (i < 10) {
+          results.push(...val);
+          return;
+        }
+
+        // 将负数插入数组前面
+        results.unshift(...val);
+        return;
+      }
+
+      if (i < 10) {
+        results.unshift(...val);
+        return;
+      }
+
+      // 将负数插入数组前面
+      results.push(...val);
+    });
+
+    return results;
+  };
+
+  let max = getValue(arr.reduce((a, b) => (getValue(a) > getValue(b) ? a : b)));
+
+  let significantDigit = 1;
+  let newArr = arr;
+  while (Math.abs(max / significantDigit) >= 1) {
+    newArr = countsRadixSort(newArr, significantDigit, getValue, isAsc);
+    significantDigit *= 10;
+  }
+
+  return newArr;
+};
