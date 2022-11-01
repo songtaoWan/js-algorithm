90cba4ac66a71abcdc799dd326fdbf9bc6036a6f
wansongtao <18101837209@163.com>
2022-11-01 09:44:02 +0800

    perf:希尔排序优化

--- chapter13/shellSort.ts
+++ chapter13/shellSort.ts
@@ -1,19 +1,21 @@
 import { compare, getChaoticArray } from './test';
 
 /**
- * 希尔排序
+ * 希尔排序，改变原数组
  * @param arr 
  * @param compareFn a > b 升序，b > a 降序
  * @returns 
  */
 export default function shellSort<T = unknown>(arr: T[], compareFn: (a: T, b: T) => boolean) {
   const length = arr.length;
-  if (!length) {
-    return [];
+  if (length < 2) {
+    return arr;
   }
 
+  // 设置步长，一个好的步长可以提高算法速度
   let spacing = Math.floor(length / 3);
   do {
+    // 将数组的每一项与其前n项比较
     for (let i = 1; i < length; i++) {
       let prevIdx = i - spacing;
       const currVal = arr[i];
