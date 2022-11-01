0b2114b3510716fb87197bb2b6cd81da0fb42eea
wansongtao <18101837209@163.com>
2022-11-01 09:39:02 +0800

    feat:插入排序js版

--- chapter13/test/sort.js
+++ chapter13/test/sort.js
@@ -45,7 +45,7 @@ const getChaoticArray = (len = 10, min = 10, max = 100) => {
 };
 
 /**
- * 冒泡排序
+ * 冒泡排序，改变原数组
  * @param {any[]} arr
  * @param {(a, b) => boolean} compareFn
  * @returns
@@ -92,6 +92,35 @@ const selectionSort = (arr, compareFn) => {
     if (i !== idx) {
       [arr[i], arr[idx]] = [arr[idx], arr[i]];
     }
+  }
+
+  return arr;
+};
+
+/**
+ * 插入排序，改变原数组
+ * @param {any[]} arr
+ * @param {(a, b) => boolean} compareFn
+ * @returns
+ */
+const insertionSort = (arr, compareFn) => {
+  const length = arr.length;
+  if (length < 2) {
+    return arr;
+  }
+
+  // 将数组的每一项与其前面的每一个元素比较
+  for (let i = 1; i < length; i++) {
+    let prevIdx = i - 1;
+    const currVal = arr[i];
+
+    while (prevIdx >= 0 && compareFn(arr[prevIdx], currVal)) {
+      arr[prevIdx + 1] = arr[prevIdx];
+      prevIdx--;
+    }
+
+    prevIdx++;
+    arr[prevIdx] = currVal;
   }
 
   return arr;
