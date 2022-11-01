7351242c873a4d9b42b2c094eaaab47e3b2a4824
wansongtao <18101837209@163.com>
2022-11-01 09:34:28 +0800

    perf:选择排序优化

--- chapter13/selectionSort.ts
+++ chapter13/selectionSort.ts
@@ -1,16 +1,23 @@
 import { compare, getChaoticArray } from "./test";
 
+/**
+ * 选择排序，改变原数组
+ * @param arr 
+ * @param compareFn 
+ * @returns 
+ */
 export default function selectionSort<T = unknown>(
   arr: T[],
   compareFn: (a: T, b: T) => boolean
 ) {
   const length = arr.length;
-  if (!length) {
-    return [];
+  if (length < 2) {
+    return arr;
   }
 
   for (let i = 0; i < length - 1; i++) {
     let idx = i;
+    // 寻找最大（小）值索引，最大还是最小取决于你的比较函数
     for (let j = i + 1; j < length; j++) {
       if (compareFn(arr[idx], arr[j])) {
         idx = j;
