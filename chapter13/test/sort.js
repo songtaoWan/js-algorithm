79b12907f6d7d571d2039f4566e20a3e0ca39fce
wansongtao <18101837209@163.com>
2022-11-01 09:44:18 +0800

    feat:希尔排序js版

--- chapter13/test/sort.js
+++ chapter13/test/sort.js
@@ -125,3 +125,37 @@ const insertionSort = (arr, compareFn) => {
 
   return arr;
 };
+
+/**
+ * 希尔排序，改变原数组
+ * @param {any[]} arr
+ * @param {(a, b) => boolean} compareFn
+ * @returns
+ */
+const shellSort = (arr, compareFn) => {
+  const length = arr.length;
+  if (length < 2) {
+    return arr;
+  }
+
+  // 设置步长，一个好的步长可以提高算法速度
+  let spacing = Math.floor(length / 3);
+  do {
+    // 将数组的每一项与其前n项比较
+    for (let i = 1; i < length; i++) {
+      let prevIdx = i - spacing;
+      const currVal = arr[i];
+
+      while (prevIdx >= 0 && compareFn(arr[prevIdx], currVal)) {
+        arr[prevIdx + spacing] = arr[prevIdx];
+        prevIdx -= spacing;
+      }
+
+      arr[prevIdx + spacing] = currVal;
+    }
+
+    spacing = Math.floor(spacing / 3);
+  } while (spacing >= 1);
+
+  return arr;
+};
